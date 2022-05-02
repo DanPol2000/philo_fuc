2799301c9eaec6b2957f0295f112275a4b7c492d
DanPol2000 <89585637+DanPol2000@users.noreply.github.com>
2022-05-02 15:33:28 +0300

    Add files via upload

--- philosophs/philo_bonus/action.c
+++ philosophs/philo_bonus/action.c
@@ -6,7 +6,7 @@
 /*   By: chorse <chorse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 15:08:12 by ysachiko          #+#    #+#             */
-/*   Updated: 2022/05/01 23:43:54 by chorse           ###   ########.fr       */
+/*   Updated: 2022/05/02 15:27:32 by chorse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,41 +24,31 @@ static void	take_forks(t_data *data, int id)
 
 int	check_death(t_data *data)
 {
-	printf("RAZ = %lld\nID = %d\n", data->last_meal - data->zero_time, data->id);
+	sem_wait(data->print);
+	// printf("ALOS = %lld\n", ft_time() - data->last_meal);
 	if (ft_time() - data->last_meal > data->die_time)
 	{
 		printf("Философ сдох\n");
 		exit(1);
 	}
+	sem_post(data->print);
 	return (0);
 }
 
 void	ft_action(t_data *data, int id)
 {
+	// update_time(data);
 	while (1)
 	{
-		// printf("Here2 = %d\n", data->id);
-		// printf("Other = %d\n", id);
-		// if (ft_time() - data->last_meal > data->die_time)
-		// 	exit(1);
-		// check_death(data);
 		take_forks(data, id);
-		usleep(300);
 		check_death(data);
-		// if (ft_time() - data->last_meal > data->die_time)
-		// 	exit(1);
-		// check_death(data);
 		eating(data, id);
-		// if (ft_time() - data->last_meal > data->die_time)
-		// 	exit(1);
-		// check_death(data);
 		sleeping(data, id);
 		if (data->eat != -1)
 			data->cycles--;
 		if (data->cycles == 0)
 			exit (0);
 	}
-	printf("Философ сдох\n");
 	exit (1);
 }
 
