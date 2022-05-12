b7c41b0fb56c38164edc639f8e0da47f31995c81
DanPol2000 <89585637+DanPol2000@users.noreply.github.com>
2022-05-12 16:00:23 +0300

    Add files via upload

---
+++ philo_bonus/action.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   action.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chorse <chorse@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 15:08:12 by ysachiko          #+#    #+#             */
+/*   Updated: 2022/05/12 15:38:07 by chorse           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_bonus.h"
+
+static void	take_forks(t_data *data, int id)
+{
+	sem_wait(data->general);
+	sem_wait(data->forks);
+	ft_print_msg(data, 'f', id);
+	if (data->number == 1)
+	{
+		while (1)
+			check_death(data);
+	}
+	sem_wait(data->forks);
+	ft_print_msg(data, 'f', id);
+	sem_post(data->general);
+}
+
+int	check_death(t_data *data)
+{
+	if (ft_time() - data->last_meal > data->die_time)
+	{
+		sem_wait(data->print);
+		printf("%lld %d died\n", \
+		ft_time() - data->zero_time, data->id + 1);
+		exit(1);
+	}
+	return (0);
+}
+
+void	ft_action(t_data *data, int id)
+{
+	// update_time(data);
+	while (1)
+	{
+		take_forks(data, id);
+		eating(data, id);
+		sleeping(data, id);
+		if (data->eat != -1)
+			data->cycles--;
+		if (data->cycles == 0)
+			exit (0);
+	}
+	exit (1);
+}
+
+void	eating(t_data *data, int id)
+{
+	ft_print_msg(data, 'e', id);
+	ft_sleep(data->eat_time);
+	check_death(data);
+	data->last_meal = ft_time();
+	sem_post(data->forks);
+	sem_post(data->forks);
+}
+
+void	sleeping(t_data *data, int id)
+{
+	ft_print_msg(data, 's', id);
+	ft_sleep(data->sleep_time);
+	ft_print_msg(data, 't', id);
+}
